4e6d87c312aae56f726d87d297f110a9169464b3
FominDenisAn <52245335+FominDenisAn@users.noreply.github.com>
2019-06-30 15:47:39 +0300

    Update jquery.mousewheel.js

--- js/jquery.mousewheel.js
+++ js/jquery.mousewheel.js
@@ -1,14 +1,3 @@
-/*! Copyright (c) 2009 Brandon Aaron (http://brandonaaron.net)
- * Dual licensed under the MIT (http://www.opensource.org/licenses/mit-license.php)
- * and GPL (http://www.opensource.org/licenses/gpl-license.php) licenses.
- * Thanks to: http://adomas.org/javascript-mouse-wheel/ for some pointers.
- * Thanks to: Mathias Bank(http://www.mathias-bank.de) for a scope bug fix.
- *
- * Version: 3.0.2
- * 
- * Requires: 1.2.2+
- */
-
 (function($) {
 
 var types = ['DOMMouseScroll', 'mousewheel'];
