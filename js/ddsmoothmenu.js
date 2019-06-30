49cfeeba5beb0464ca5c75b31b3165ef8bd03ab4
FominDenisAn <52245335+FominDenisAn@users.noreply.github.com>
2019-06-30 15:45:42 +0300

    Update ddsmoothmenu.js

--- js/ddsmoothmenu.js
+++ js/ddsmoothmenu.js
@@ -1,28 +1,3 @@
-//** Smooth Navigational Menu- By Dynamic Drive DHTML code library: http://www.dynamicdrive.com
-//** Script Download/ instructions page: http://www.dynamicdrive.com/dynamicindex1/ddlevelsmenu/
-//** Menu created: Nov 12, 2008
-
-//** Dec 12th, 08" (v1.01): Fixed Shadow issue when multiple LIs within the same UL (level) contain sub menus: http://www.dynamicdrive.com/forums/showthread.php?t=39177&highlight=smooth
-
-//** Feb 11th, 09" (v1.02): The currently active main menu item (LI A) now gets a CSS class of ".selected", including sub menu items.
-
-//** May 1st, 09" (v1.3):
-//** 1) Now supports vertical (side bar) menu mode- set "orientation" to 'v'
-//** 2) In IE6, shadows are now always disabled
-
-//** July 27th, 09" (v1.31): Fixed bug so shadows can be disabled if desired.
-//** Feb 2nd, 10" (v1.4): Adds ability to specify delay before sub menus appear and disappear, respectively. See showhidedelay variable below
-
-var ddsmoothmenu={
-
-//Specify full URL to down and right arrow images (23 is padding-right added to top level LIs with drop downs):
-arrowimages: {down:['downarrowclass', 'down.gif', 23], right:['rightarrowclass', 'right.gif']},
-transition: {overtime:300, outtime:300}, //duration of slide in/ out animation, in milliseconds
-shadow: {enable:true, offsetx:5, offsety:5}, //enable shadow?
-showhidedelay: {showdelay: 100, hidedelay: 200}, //set delay in milliseconds before sub menus appear and disappear, respectively
-
-///////Stop configuring beyond here///////////////////////////
-
 detectwebkit: navigator.userAgent.toLowerCase().indexOf("applewebkit")!=-1, //detect WebKit browsers (Safari, Chrome etc)
 detectie6: document.all && !window.XMLHttpRequest,
 
